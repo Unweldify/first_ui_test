ebc6e398e05b0a0b5878ca9608d62e0fe83ab7d1
Unweldify <AILALAH@yandex.ru>
2024-11-18 19:20:40 +0400

    Made it work

--- pages.py
+++ pages.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
+from typing import List
 
 
 class BasePage:
@@ -11,11 +12,11 @@ class BasePage:
         self.wait = WebDriverWait(driver, timeout)
         self.page_url = ''
 
-    def find_element(self, by: By or int, value: str) -> WebElement:
+    def find_element(self, by: By | int, value: str) -> WebElement:
         return self.wait.until(expected_conditions.visibility_of_element_located((by, value)),
                                message=f'Элемент {by, value} не найден')
 
-    def find_elements(self, by: By or int, value: str) -> [WebElement]:
+    def find_elements(self, by: By | int, value: str) -> List[WebElement]:
         return self.wait.until(expected_conditions.visibility_of_all_elements_located((by, value)),
                                message=f'Элементы {by, value} не найдены')
 
@@ -27,9 +28,9 @@ class LoginPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver, timeout=60)
 
-        self.login = (By.ID, 'user-name')           #Локатор по ID для элемента строки ввода login
-        self.password = (By.ID, 'password')         #Локатор по ID для элемента строки ввода password
-        self.login_btn = (By.NAME, 'login-button')  #Локатор по Name для элемента кнопка Login
+        self.login = (By.ID, 'user-name')
+        self.password = (By.ID, 'password')
+        self.login_btn = (By.NAME, 'login-button')
 
     def auth(self, login: str, password: str) -> None:
         self.find_element(*self.login).send_keys(login)
@@ -41,39 +42,39 @@ class InventoryPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver, timeout=60)
 
-        self.item = #Заголовок любого товара: Здесь будет локатор по ID
-        self.add_jacket_to_catr_btn = #Кнопка Add to Cart для Sauce Labs Fleece Jacket: Здесь будет локатор XPATH
-        self.cart_btn = #Кнопка корзины: Здесь будет локатор XPATH
+        self.item = (By.ID, 'item_4_title_link')
+        self.add_jacket_to_cart_btn = (By.XPATH, '//*[@id="add-to-cart-sauce-labs-fleece-jacket"]')
+        self.cart_btn = (By.XPATH, '//*[@class="shopping_cart_link"]')
 
-    def choose_item(self):
+    def choose_item(self) -> None:
         self.find_element(*self.item).click()
 
-    def add_jacket_to_cart_btn_click(self):
-        self.find_elements(*self.add_jacket_to_catr_btn).click()
+    def add_jacket_to_cart_btn_click(self) -> None:
+        self.find_element(*self.add_jacket_to_cart_btn).click()
 
-    def cart_btn_click(self):
+    def cart_btn_click(self) -> None:
         self.find_element(*self.cart_btn).click()
 
 class ItemPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver, timeout=60)
 
-        self.add_to_cart_btn = #Кнопка Add to Cart: Здесь будет локатор XPATH
-        self.back_to_products = #Кнопка Back to products: Здесь будет локатор XPATH
+        self.add_to_cart_btn = (By.XPATH, '//*[@id="add-to-cart"]') #Кнопка Add to Cart: Здесь будет локатор XPATH
+        self.back_to_products = (By.XPATH, '//*[@id="back-to-products"]') #Кнопка Back to products: Здесь будет локатор XPATH
 
-    def add_to_cart_btn_click(self):
+    def add_to_cart_btn_click(self) -> None:
         self.find_element(*self.add_to_cart_btn).click()
 
-    def back_to_products(self):
-        self.find_element(*self.back_to_products()).click()
+    def back_to_products_click(self) -> None:
+        self.find_element(*self.back_to_products).click()
 
 class CartPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver, timeout=60)
 
-        self.item_list =    #Локатор XPATH элемента продукта. Локатор должен находить
+        self.item_list =  (By.XPATH, '(//*[@class="cart_item"])') #Локатор XPATH элемента продукта. Локатор должен находить
                             # ровно 2 элемента на странице: первый и второй товар
                             # то есть в DevTools вы должны видеть "1 of 2" при поиске данного локатора
 
-    def number_of_products(self):
+    def number_of_products(self) -> int:
         return len(self.find_elements(*self.item_list))
